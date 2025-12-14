82d38688fed926e0af8f2cc33e6cc0bf67a2580b
Denise da Cunha Silva <deni_cunha@hotmail.com>
2025-12-14 15:08:48 -0300

    Refactor:
    Regorganização da estrutura de pastas e adequação aos conceitos passados em sala de aula

---
+++ src/etl/pipeline.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '../../')))
+
+from extract import *
+from transform import *
+from load import *
+
+def pipeline_bricks_e_filiais():
+    #1. Extract
+    df_filial_brick = extract_filial_brick_data_xlsx('../../data/raw/filial-brick_sample.xlsx')
+    print('Coleta de filiais por brick efetuada com sucesso!')
+
+    #2. Transform
+    df_filial_brick_clean = limpar_filial_brick_data_xlsx(df_filial_brick)
+    print('Transformação concluída com sucesso!')
+    print(df_filial_brick_clean.head())
+    #3. Load
+    #carregar_bricks_e_filiais(df_filial_brick_clean)
+    #print('Carga concluída com sucesso!')
+
+if __name__ == "__main__":
+    pipeline_bricks_e_filiais()
