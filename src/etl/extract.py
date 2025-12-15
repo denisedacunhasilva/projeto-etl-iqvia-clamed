3d4642f906ed67948309edcc4b7e426aba8ec70a
Denise da Cunha Silva <deni_cunha@hotmail.com>
2025-12-15 13:40:05 -0300

    Fix:
    Ajustes extract.py

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -1,42 +1,15 @@
 import pandas as pd
 import numpy as np
+import os
+import sys
 from src.database import connect_db
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '../../')))
 
 def extrair_filial_brick_data_xlsx(path: str) -> pd.DataFrame:
     df = pd.read_excel(path, dtype = {
-        'filial_brick_id': int,
-        'filial_id': int,
-        'brick_id': int,
-        #'data_criacao': (datedate_format='%d%m-%Y-'),
-        'ativo': bool
+        'brick': str,
+        'cod_filial': int
     })
 
     return df
-
-
-
-############################################################
-
-'''
-
-import pandas as pd
-from pathlib import Path
-
-
-def extract_iqvia_data(file_path: Path) -> pd.DataFrame:
-    try:
-        df = pd.read_excel(file_path)
-        print(f"✔ IQVIA carregado | Linhas: {df.shape[0]} | Colunas: {df.shape[1]}")
-        return df
-    except Exception as e:
-        raise RuntimeError(f"Erro na extração IQVIA: {e}")
-
-
-def extract_filial_brick_data(file_path: Path) -> pd.DataFrame:
-    try:
-        df = pd.read_excel(file_path)
-        print(f"✔ Filial-Brick carregado | Linhas: {df.shape[0]} | Colunas: {df.shape[1]}")
-        return df
-    except Exception as e:
-        raise RuntimeError(f"Erro na extração Filial-Brick: {e}")
-'''
