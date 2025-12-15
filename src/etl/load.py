574a7fbfd796b3c14fae1ac1fe3243094453086b
Denise da Cunha Silva <deni_cunha@hotmail.com>
2025-12-15 18:07:05 -0300

    Refactor:
    Refatoração de código

--- src/etl/load.py
+++ src/etl/load.py
@@ -8,8 +8,14 @@ from src.database import cadastrar_dim_brick
 import pandas as pd
 
 def carregar_bricks_e_filiais(df_filial_brick_clean: pd.DataFrame):
+    
+    df_bricks = (
+        df_filial_brick_clean[['cod_brick', 'nome_brick']]
+        .drop_duplicates()
+    )
+  
 
-    for _, row in df_filial_brick_clean.iterrows():
+    for _, row in df_bricks.iterrows():
         cadastrar_dim_brick(row['cod_brick'], 
                             row['nome_brick']
                             )
