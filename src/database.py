8d3386aa65e71b3dbfbb1322066e17d46ffacf44
Denise da Cunha Silva <deni_cunha@hotmail.com>
2025-12-15 17:30:01 -0300

    Fix:
    Atualização database.py

--- src/database.py
+++ src/database.py
@@ -12,3 +12,21 @@ def connect_db(dbname, user, password, port, host):
         )
     except:
         print("Erro: Falha ao conectar com o banco de dados!\n")
+
+def cadastrar_dim_brick(cod_brick: str, nome_brick: str):
+    '''
+        Cadastra um novo brick no banco de dados.
+
+        Parâmetros:
+            - cod_brick: str
+            - nome_brick: str
+    '''
+    with connect_db('clamed', 'postgres', '@m@R3linh0', 5432, 'localhost') as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute('INSERT INTO dim_brick(cod_brick, nome_brick) VALUES (%s, %s)',
+                            (cod_brick, nome_brick))
+            except Exception as e:
+                print('Error: Falha ao cadastrar dim_brick! ', e)
+
+    
